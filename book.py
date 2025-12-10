5f7998ac48c61a64dd2e7e60daea53e30b47f8f5
PunishedPlebe <mylescharlesworth@icloud.com>
2025-12-10 13:54:15 -0500

    Created Basic Type Checking for Book Object Constructor and Setters to Ensure Normalized Data Structure

--- book.py
+++ book.py
@@ -1,5 +1,19 @@
 class Book:
-    def __init__(self, title, author, publish_date, genre, ISBN, read_status=False, ID):
+    def __init__(self, title, author, publish_date, genre, ISBN, read_status=False, ID=1):
+        if not isinstance(title, str):
+            raise TypeError("Title Value Must Be A String")
+        if not isinstance(author, str):
+            raise TypeError("Author Value Must Be A String")
+        if not isinstance(publish_date, str):
+            raise TypeError("Publish Date Value Must Be A String")
+        if not isinstance(genre, str):
+            raise TypeError("genre Value Must Be A String")
+        if not isinstance(ISBN, int):
+            raise TypeError("ISBN Value Must Be A Int")
+        if not isinstance(read_status, bool):
+            raise TypeError("Read Status Value Must Be A Bool")
+        if not isinstance(ID, int):
+            raise TypeError("ID Value Must Be A Int")
         self.__title = title
         self.__author = author
         self.__date = publish_date
@@ -12,40 +26,57 @@ class Book:
         return self.__title
 
     def set_title(self, new_title):
+        if not isinstance(new_title, str):
+            raise TypeError("Title Must Be Type String")
         self.__title = new_title
     
     def get_author(self):
         return self.__author
 
     def set_author(self, new_author):
+        if not isinstance(new_author, str):
+            raise TypeError("Author Must Be Type String")
         self.__author = new_author
     
     def get_publish_date(self):
         return self.__date
 
     def set_publish_date(self, new_date):
+        if not isinstance(new_date, str):
+            raise TypeError("Date Must Be Type String")
         self.__date = new_date
 
     def get_genre(self):
         return self.__genre
 
     def set_genre(self, new_genre):
+        if not isinstance(new_genre, str):
+            raise TypeError("Genre Must Be Type String")
         self.__genre = new_genre
     
     def get_isbn(self):
         return self.__isbn
     
     def set_isbn(self, new_ISBN):
+        if not isinstance(new_ISBN, int):
+            raise TypeError("ISBN Must Be Type Int")
         self.__isbn = new_ISBN
     
     def get_read_status(self):
         return self.__read_status
     
     def set_read_status(self, new_flag):
+        if not isinstance(new_flag, bool):
+            raise TypeError("Read Status Must Be Type Bool")
         self.__read_status = new_flag
     
     def get_id(self):
         return self.__id
     
     def set_id(self, new_id):
+        if not isinstance(new_id, int):
+            raise TypeError("ID Must Be Type Int")
         self.__id = new_id
+
+    def __str__(self):
+        return(f"Title: {self.__title}, Author: {self.__author}, Publish Date: {self.__date}, Genre: {self.__genre}, ISBN: {self.__isbn}, Have Read?: {self.__read_status}, ID: {self.__id}")
---
+++ book_test.py
@@ -0,0 +1,75 @@
+from book import Book
+
+def book_test():
+    print("Welcome to The Book Object Test Module")
+    print("======================================")
+    print("Testing Book Class Constructor...")
+    print("======================================")
+    master_test_book = Book("The Alchemist", "Paulo Coelho", "1988", "Adventure/Philosophical", 9780061122415)
+    print(master_test_book)
+    print("======================================")
+    print("Testing Constructor Type Exceptions...")
+    print("======================================")
+    try:
+        test_book1 = Book(987, "Test Author", "Test Date", "Test Genre", 123456789, False, 1) # attempting to trip title string exception
+    except Exception as e:
+        print(e)
+    try:
+        test_book1 = Book("Test Title", 123, "Test Date", "Test Genre", 123456789, False, 1) # attempting to trip author string exception
+    except Exception as e:
+        print(e)
+    try:
+        test_book1 = Book("Test Title", "Test Author", 123, "Test Genre", 123456789, False, 1) # attempting to trip date string exception
+    except Exception as e:
+        print(e)
+    try:
+        test_book1 = Book("Test Title", "Test Author", "Test Date", 123, 123456789, False, 1) # attempting to trip genre string exception
+    except Exception as e:
+        print(e)
+    try:
+        test_book1 = Book("Test Title", "Test Author", "Test Date", "Test Genre", "123456789", False, 1) # attempting to trip ISBN Int exception
+    except Exception as e:
+        print(e)
+    try:
+        test_book1 = Book("Test Title", "Test Author", "Test Date", "Test Genre", 123456789, "False", 1) # attempting to trip Read Status bool exception
+    except Exception as e:
+        print(e)
+    try:
+        test_book1 = Book("test Title", "Test Author", "Test Date", "Test Genre", 123456789, False, "6") # attempting to trip book id int exception
+    except Exception as e:
+        print(e)
+    print("======================================")
+    print("Testing Setter Type Exceptions...")
+    print("======================================")
+    test_book2 = master_test_book
+    try:
+       test_book2.set_title(123)
+    except Exception as e:
+        print(e)
+    try:
+       test_book2.set_author(123)
+    except Exception as e:
+        print(e)
+    try:
+       test_book2.set_publish_date(123)
+    except Exception as e:
+        print(e)
+    try:
+       test_book2.set_genre(123)
+    except Exception as e:
+        print(e)
+    try:
+       test_book2.set_isbn("123")
+    except Exception as e:
+        print(e)
+    try:
+       test_book2.set_read_status("False")
+    except Exception as e:
+        print(e)
+    try:
+       test_book2.set_id("123")
+    except Exception as e:
+        print(e)
+
+
+book_test()
